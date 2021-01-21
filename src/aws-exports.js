60f7144c70fd80b60be3da803b29daf80d70a73c
theopposablethumb <brendan.meachen@gmail.com>
2021-01-21 22:24:22 +0000

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -16,8 +16,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/",
-        "redirectSignOut": "http://localhost:3000/",
+        "redirectSignIn": "https://main.d3ihtjzvgz37vw.amplifyapp.com/",
+        "redirectSignOut": "https://main.d3ihtjzvgz37vw.amplifyapp.com/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS"
