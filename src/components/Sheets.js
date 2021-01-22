e2cebccae0a6e631f5e1131243012629704ac568
theopposablethumb <brendan.meachen@gmail.com>
2021-01-22 11:41:50 +0000

    Sheets are not posting on AWS, debugging

--- src/components/Sheets.js
+++ src/components/Sheets.js
@@ -6,6 +6,7 @@ let date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
 let FetchSpreadSheet = (name, email, token, team, values) => {
     values.unshift(date, name, email);
     let sheet = team;
+    console.log(token);
 
     fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}/values/${sheet}!A1:X1:append?valueInputOption=USER_ENTERED`, {
         method: "POST",
