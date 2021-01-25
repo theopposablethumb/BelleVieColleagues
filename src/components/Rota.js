e2d34606a2a18de7b9ee3f6923ef3837040f49ed
theopposablethumb <brendan.meachen@gmail.com>
2021-01-25 16:34:55 +0000

    React error 152

--- src/components/Rota.js
+++ src/components/Rota.js
@@ -1,7 +1,7 @@
 import {ReadSpreadSheet} from '../api/Sheets';
 
 let Rota = (props) => {
-    ReadSpreadSheet(props.user.attributes.website);
+    return ReadSpreadSheet(props.user.attributes.website);
 }
 
 export default Rota;
