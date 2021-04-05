1b6b3eb8014520ddef23e0857521129ece5287a0
theopposablethumb <brendan.meachen@gmail.com>
2021-04-05 15:22:46 +0100

    Prepping for prod

--- src/common/Navigation.js
+++ src/common/Navigation.js
@@ -18,8 +18,8 @@ class Navigation extends React.Component {
                 <button onClick={(e) => this.openNav(e)} className={this.state.open ? 'menu active' : 'menu close'}></button>
                 <nav className={this.state.open ? 'open' : 'close'}>
                     <NavLink exact to="/">Confirmation Practices</NavLink>
-                    <NavLink to="/reporting-hours">Reporting Hours</NavLink>
-                    <NavLink to="/annual-leave">Annual Leave</NavLink>
+                    {/*<NavLink to="/reporting-hours">Reporting Hours</NavLink>
+                    <NavLink to="/annual-leave">Annual Leave</NavLink> */}
                     <NavLink to="/faqs">FAQs</NavLink>
                     <NavLink to="/policies">Policies</NavLink>
                     <NavLink to="/rota">Rota Prototype</NavLink>
