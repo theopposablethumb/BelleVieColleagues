acccd5f939b545daf85abdc84e86da3c926f4490
theopposablethumb <brendan.meachen@gmail.com>
2021-05-11 16:56:40 +0100

    Env changes

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -28,7 +28,7 @@ const awsmobile = {
             "region": "eu-west-2"
         }
     ],
-    "aws_appsync_graphqlEndpoint": "https://4qse55lncbahhlaalj74apxbfu.appsync-api.eu-west-2.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://mvzq4xizqzhr3lxvw7xvqicv5m.appsync-api.eu-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "eu-west-2",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
 };
