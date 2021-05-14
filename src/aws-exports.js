11a5b7438683de7b72fa33f39d3036773505d34f
theopposablethumb <brendan.meachen@gmail.com>
2021-05-14 14:05:04 +0100

    staging back end

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -27,10 +27,7 @@ const awsmobile = {
             "endpoint": "https://slzdx9uvk8.execute-api.eu-west-2.amazonaws.com/staging",
             "region": "eu-west-2"
         }
-    ],
-    "aws_appsync_graphqlEndpoint": "https://mvzq4xizqzhr3lxvw7xvqicv5m.appsync-api.eu-west-2.amazonaws.com/graphql",
-    "aws_appsync_region": "eu-west-2",
-    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
+    ]
 };
 
 
