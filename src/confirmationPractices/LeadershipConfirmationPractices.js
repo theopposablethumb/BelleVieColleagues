e49d1e73ec50d7fd34814fc046619906dc0a997f
theopposablethumb <brendan.meachen@gmail.com>
2021-05-14 13:46:52 +0100

    Added new home page and LeaderShip CP component

---
+++ src/confirmationPractices/LeadershipConfirmationPractices.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+class LeadershipConfirmationPractices extends React.Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default LeadershipConfirmationPractices;
